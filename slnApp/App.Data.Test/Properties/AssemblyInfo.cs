3657ac3a0e9827b83c1722b5cf8e9c92c3fe71a1
ecastillo0292 <e.castillo0292@gmail.com>
2019-04-14 12:07:28 -0500

    proyecto semana 4

---
+++ slnApp/App.Data.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App.Data.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App.Data.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4b03779-0ce3-472b-8a86-299d15e0ba0c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ slnApp/App.Data/ArtistDA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data
+{
+    public class ArtistDA:BaseConnection
+    {
+        /// <summary>
+        /// Permite Obtener la cantidad de registros
+        /// que existen en la tabla de artista
+        /// </summary>
+        /// <returns>Retorna el número de registros</returns>
+        public int GetCound()
+        {
+            var result = 0;
+
+            var sql = "SELECT COUNT(1) FROM Artist";
+            /*1.- Creando la instancia del objeto connection*/
+            using (IDbConnection cn = new SqlConnection(this.ConnectionString))
+            {
+                /*2.- Creando el objeto command */
+                IDbCommand cmd = new SqlCommand(sql);
+                cmd.Connection = cn;
+
+                cn.Open(); // Abriendo la conexión a la base de datos
+
+                result = (int)cmd.ExecuteScalar();
+
+            }
+
+
+            return result;
+        }
+
+    }
+}
